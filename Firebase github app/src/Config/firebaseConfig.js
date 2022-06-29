c80687290f48ce5c6694802607a379b097c9692e
ajithkumaar <107831207+ajithkumaar@users.noreply.github.com>
2022-06-29 19:10:16 +0530

    Add files via upload

---
+++ Firebase github app/src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAze9w3FGVKavbQQXi6tWCrjePPI36vzVI",
+    authDomain: "mygitapp-40e06.firebaseapp.com",
+    projectId: "mygitapp-40e06",
+    storageBucket: "mygitapp-40e06.appspot.com",
+    messagingSenderId: "1032876214455",
+    appId: "1:1032876214455:web:947f9be5f97495b7c66532",
+    measurementId: "G-9PXJNMV4FQ"
+  };
